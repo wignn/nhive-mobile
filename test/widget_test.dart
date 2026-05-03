c7a5d24a1cddc5e1a8163904791a31d699a8a477
wignn <tigfiragnafatur1933@gmail.com>
2026-05-03 12:14:48 +0700

    refactor: clean up build and widget test files by removing unused imports and unnecessary lines

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:nhive/app/app.dart';
 import 'package:nhive/core/di/service_locator.dart';
 import 'package:nhive/features/auth/presentation/pages/login_page.dart';
 
